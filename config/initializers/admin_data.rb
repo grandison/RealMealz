cf1d1034ef5a7394b82dfa07b2d5d893368450e6
Max Dunn <maxdunn@Maxs-MacBook-Pro.local>
2012-01-23 08:38:17 -0800

    Fix admin_data

--- config/initializers/admin_data.rb
+++ config/initializers/admin_data.rb
@@ -1,5 +1,5 @@
 AdminData.config do |config|
   #Disable their security because we do our own
-  config.is_allowed_to_view = true 
-  config.is_allowed_to_update = true 
+  config.is_allowed_to_view = lambda {|controller| true }
+  config.is_allowed_to_update = lambda {|controller| true }
 end
