a3d5971e577bdc204e2d8b3a12b816a1770e41a4
Max Dunn <maxdunn@Maxs-MacBook-Pro.local>
2012-02-03 11:50:38 -0800

    Bug 191 - API Method to retrieve My Meals Data

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -10,6 +10,14 @@ class MealsController < ApplicationController
     current_user.kitchen.get_sorted_my_meals.each do |meal|
       next if meal.recipe_id.nil?
       
+      ingredients = []
+      meal.recipe.ingredients_recipes.each do |ir|
+        next if ir.ingredient_id.nil?
+        ingredients << {:ingredient_name => ir.ingredient.name,
+          :unit => ir.unit, :weight => ir.weight, :description => ir.description,
+          :name_and_unit => ir.name}
+      end
+      
       meals << {:recipe_id => meal.recipe_id, :recipe_name => meal.recipe.name, 
         :recipe_intro => meal.recipe.intro,
         :recipe_prepsteps => meal.recipe.prepsteps, :recipe_cooksteps => meal.recipe.cooksteps,
@@ -18,9 +26,7 @@ class MealsController < ApplicationController
         :recipe_prep_time => meal.recipe.preptime, :recipe_cook_time => meal.recipe.cooktime,  
         :picture_thumb => meal.recipe.picture(:thumb), 
         :picture_medium =>  meal.recipe.picture(:medium),
-        :ingredients => meal.recipe.ingredients_recipes.map {|ir| {:ingredient_name => ir.ingredient.name,
-          :unit => ir.unit, :weight => ir.weight, :description => ir.description,
-          :name_and_unit => ir.name} }
+        :ingredients => ingredients
       }
     end
 
