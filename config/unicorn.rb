463741e13b00542927a57a4b98891d5185eccbe6
Max Dunn <maxdunn@Maxs-MacBook-Pro.local>
2012-02-06 14:55:22 -0800

    preload_app true   # Needed for NewRelic and Heroku, otherwise not data is logged

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,3 +1,9 @@
 worker_processes 3 # amount of unicorn workers to spin up
 timeout 30         # restarts workers that hang for 30 seconds
 preload_app true   # Needed for NewRelic and Heroku, otherwise not data is logged
+
+before_fork do |server, worker|
+  # the following is highly recomended for Rails + "preload_app true"
+  # as there's no need for the master process to hold a connection
+  defined?(ActiveRecord::Base) and ActiveRecord::Base.connection.disconnect!
+end
