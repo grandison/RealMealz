037fd86c9082a1201f2327bab496c154b65d96af
Max Dunn <max@maxdunn.com>
2012-03-17 21:25:12 -0700

    Add /track/track_points for SuperAdmins to list of points for each user

--- app/controllers/track_controller.rb
+++ app/controllers/track_controller.rb
@@ -1,6 +1,6 @@
 class TrackController < ApplicationController
 
-  skip_before_filter :require_super_admin
+  skip_before_filter :require_super_admin, :except => :point_tracker
   before_filter :require_user
 
   #---------------------------------
@@ -8,6 +8,7 @@ class TrackController < ApplicationController
     @food_balance = Balance.get_kitchen_balance(current_user.kitchen)
     @target_food_balance = current_user.get_target_food_balance
     @points = current_user.points.group_by(&:description)
+    @teams = current_user.teams
     @background_recipe = Recipe.random_background_image
   end
   
@@ -15,6 +16,11 @@ class TrackController < ApplicationController
   def save_food_balance
     current_user.update_target_food_balance(params[:newveg], params[:newstarch], params[:newprotein])
     redirect_to '/track'
+  end
+
+  #---------------------------------
+  def point_tracker
+    @points_by_group = UsersPoint.points_by_group 
   end
   
   #################
--- app/models/users_point.rb
+++ app/models/users_point.rb
@@ -1,4 +1,12 @@
 class UsersPoint < ActiveRecord::Base
   belongs_to :user
   belongs_to :point
+  
+  def self.points_by_group
+    UsersPoint.order(:user_id, :date_added).includes(:user, :point).map do |up| 
+      {:name => up.user.name,  :action => up.point.name,  :date => up.date_added}
+    end
+  end
+  
+
 end
