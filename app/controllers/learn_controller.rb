85e8f696b87d9cc33590ee5c31814e53a2ac8e75
Connie Kwan <connie@realmealz.com>
2012-03-23 18:44:45 -0700

    Updated Learn Page.

--- app/controllers/learn_controller.rb
+++ app/controllers/learn_controller.rb
@@ -5,9 +5,17 @@ class LearnController < ApplicationController
   
   def learn
     s_id = Scategory.find_by_name("technique").id
-    @techniques = Skill.find(:all, :conditions => ['video_link is not null and scategory_id = ?', s_id])
+    #@techniques = Skill.find(:all, :conditions => ['video_link is not null and scategory_id = ?', s_id])
+    @techniques1 = Skill.find(:all, :conditions => ['level = ? and scategory_id = ?', 1, s_id])
+    @techniques2 = Skill.find(:all, :conditions => ['level = ? and scategory_id = ?', 2, s_id])
+    @techniques3 = Skill.find(:all, :conditions => ['level = ? and scategory_id = ?', 3, s_id])                   
+    @techniques4 = Skill.find(:all, :conditions => ['level = ? and scategory_id = ?', 4, s_id])    
     s_id2 = Scategory.find_by_name("ingredient").id
-    @ingredients = Skill.find(:all, :conditions => ['video_link is not null and scategory_id =?', s_id2])
-    @background_recipe = Recipe.random_background_image
+    #@ingredients = Skill.find(:all, :conditions => ['video_link is not null and scategory_id =?', s_id2])
+    @ingredients1 = Skill.find(:all, :conditions => ['level = ? and scategory_id = ?', 1, s_id2])
+    @ingredients2 = Skill.find(:all, :conditions => ['level = ? and scategory_id = ?', 2, s_id2])
+    @ingredients3 = Skill.find(:all, :conditions => ['level = ? and scategory_id = ?', 3, s_id2])                   
+    @ingredients4 = Skill.find(:all, :conditions => ['level = ? and scategory_id = ?', 4, s_id2])    
+
   end  
 end
