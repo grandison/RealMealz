581cf733694a5b788b35988de41b4fb8d17a2fea
Connie Kwan <connie@realmealz.com>
2012-05-10 21:37:02 -0700

    /track page down (on master but not on local). trying to revert changes.

--- app/controllers/track_controller.rb
+++ app/controllers/track_controller.rb
@@ -8,7 +8,7 @@ class TrackController < ApplicationController
     @food_balance = Balance.get_kitchen_balance(current_user.kitchen)
     @target_food_balance = current_user.get_target_food_balance
     @points = current_user.points.group_by(&:description)
-    @group = current_user.groups.last if current_user.groups and current_user.groups.id > 9 #only show latest group  leaderboards (for PF and BlueShield)
+    @group = current_user.groups.last if current_user.groups 
     @my_team = current_user.teams.find_by_group_id(@group.id) if @group && current_user.teams
     @background_recipe = Recipe.random_background_image
   end
