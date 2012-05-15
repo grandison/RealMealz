365f7edf66d79bb4b95495152e0cf87a86dd04ea
Max Dunn <max@maxdunn.com>
2012-05-15 09:00:47 -0700

    Fix videos to use new iframe method which should work on more browsers and versions

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -4,10 +4,19 @@ class Skill < ActiveRecord::Base
     @skills ||= Skill.all
     @skills.each do |skill|
       next if skill.video_link.blank?
-      steps.gsub!("#{skill.name}","<a href='#{skill.video_link}' class='video-link', target='_blank' alt='#{skill.name}'>#{skill.name}</a>")
+      steps.gsub!("#{skill.name}","<a href='#{skill.youtube_link}' class='video-link', target='_blank' alt='#{skill.name}'>#{skill.name}</a>")
     end
     return steps
   end
+  
+  def youtube_link
+    if video_link.ends_with?('?rel=0')
+      return video_link
+    else 
+      return video_link + '?rel=0'
+    end   
+  end
+  
 end
 
 
