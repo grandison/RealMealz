c0d7c53a5e27e70beb27c5ab593a0a635b41eb0a
Max Dunn <max@maxdunn.com>
2012-12-01 11:25:43 -0800

    Bug 272 - Learn - Title on Videos are showing only the first Word

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -4,7 +4,7 @@ class Skill < ActiveRecord::Base
     @skills ||= Skill.all
     @skills.each do |skill|
       next if skill.video_link.blank?
-      steps.gsub!("#{skill.name}","<a href='#{skill.youtube_link}' class='video-link', target='_blank' alt='#{skill.name}'>#{skill.name}</a>")
+      steps.gsub!(/#{skill.name}/i,"<a href='#{skill.youtube_link}' class='video-link', target='_blank' alt='#{skill.name}'>#{skill.name}</a>")
     end
     return steps
   end
